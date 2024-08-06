9e76b9e79a47e23baae43cdb9d8b4b8e5f4be9a7
Luka <luka.luka.luka93@gmail.com>
2024-08-06 13:47:23 -0700

    toggle the class when clicked on the todo list

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 const apiURL = 'https://jsonplaceholder.typicode.com/todos';
 
 const getTodos = ()=> {
-    fetch(apiURL + '?_limit=5') // This will add on url, so it will limit in to just 5 to dos
+    fetch(apiURL + '?_limit=10') // This will add on url, so it will limit in to just 10 to dos
     .then(res => res.json())
     .then(data=> {
         data.forEach((todo)=> addTodoToDOM(todo))
@@ -12,14 +12,15 @@ const getTodos = ()=> {
 
 const addTodoToDOM = (todo) => {
     const div = document.createElement('div');
-        div.appendChild(document.createTextNode(todo.title));
-        div.setAttribute('data-id', todo.id); // with this I set each todo element ID, and its
+    div.classList.add('todo');
+    div.appendChild(document.createTextNode(todo.title));
+    div.setAttribute('data-id', todo.id); // with this I set each todo element ID, and its
 // id='1', then 2....
 
-        if(todo.completed) {
-            div.classList.add('done');
-        }
-        document.querySelector('#todo-list').appendChild(div);
+    if(todo.completed) {
+        div.classList.add('done');
+    }
+    document.querySelector('#todo-list').appendChild(div);
 }
 
 const createTodo = e => {
@@ -31,9 +32,10 @@ const createTodo = e => {
     }
 
     fetch(apiURL,{
-        method: 'POST',
-        body: JSON.stringify(newTodo),
-        headers: {
+        method: 'POST', // we want the method to be POST
+        body: JSON.stringify(newTodo), //the body we want to send it as a JSON string and then pass in the
+// the new to-do object so that will get sent in the body
+        headers: { // for headers we are gonna add an object of content, and the content we are sending is application
             'Content-Type': 'application/json'
         }
     })
@@ -41,9 +43,17 @@ const createTodo = e => {
     .then(data=> addTodoToDOM(data))
 }
 
+const toggleCompleted = e => {
+    if(e.target.classList.contains('todo')){
+        e.target.classList.toggle('done');
+    }
+}
+
 const init = () => {
     document.addEventListener('DOMContentLoaded', getTodos);
     document.querySelector('#todo-form').addEventListener('submit', createTodo);
+    document.querySelector('#todo-list').addEventListener('click', toggleCompleted);
+
 
 }
 
